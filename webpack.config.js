132ba72c076175c01ad233838ea5d3f4e9af7e99
Stuffbutts <stuffbruss@gmail.com>
2018-02-19 12:33:18 -0500

    build changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const webpack = require('webpack');
+const path = require('path');
 
 let config = {
-    entry: './src/index',
+    entry: './src/index.js',
     module: {
         rules: [
             { test: /\.js$/, use: [ 'babel-loader' ], exclude: /node_modules/ }
         ]
     },
     output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'GoogleApi.js',
         library: 'GoogleApi',
         libraryTarget: 'umd'
     },
@@ -16,12 +19,16 @@ let config = {
     },
     resolve: {
     	symlinks: false
-    }
+    },
+    devtool: 'source-map'
 };
 
 if (process.env.NODE_ENV === 'production') {
     config.plugins = [
-        new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            minimize: true
+        }),
         new webpack.LoaderOptionsPlugin({ minimize: true })
     ]
 }
